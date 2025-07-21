5cd1044e456183360393f677c30917c9783ea650
Server <admin@server.com>
2025-07-21 16:38:58 +0000

    fix backend url

--- store/contantsDataNarrator.js
+++ store/contantsDataNarrator.js
@@ -37,7 +37,7 @@ const htmlEditorToolbar = [
     ["link", "image"]
 ];
 
-const backendUrl = process.env.BACKEND_URI || "http://localhost:8000";
+const backendUrl = process.env.BACKEND_URI || "https://staging-dana-backend.elie.de";
 
 export {
     dataNarratorModes,
