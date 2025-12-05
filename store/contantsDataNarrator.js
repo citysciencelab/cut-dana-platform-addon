ff513214b75e8eb460e726adf4340bd45e5633c1
Daniel Koch <koch@terrestris.de>
2025-12-05 15:34:04 +0100

    chore: unify handling of backend url

--- store/contantsDataNarrator.js
+++ store/contantsDataNarrator.js
@@ -39,7 +39,7 @@ const htmlEditorToolbar = [
     ["link", "image"]
 ];
 
-const backendUrl = process.env.BACKEND_URI || "https://staging-dana-backend.elie.de";
+const backendUrl = Config.backendUrl || "https://staging-dana-backend.elie.de";
 
 export {
     dataNarratorModes,
