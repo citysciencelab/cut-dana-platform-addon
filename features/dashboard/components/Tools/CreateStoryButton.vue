c789e27f6c775ca0d12d0ed62cc5946061b6eea2
illsio <smasho@web.de>
2026-03-31 10:57:32 +0200

    tooltip on create story button when not logged in

--- features/dashboard/components/Tools/CreateStoryButton.vue
+++ features/dashboard/components/Tools/CreateStoryButton.vue
@@ -16,17 +16,27 @@ const createStory = async () => {
 </script>
 
 <template>
-  <v-btn
-    v-if="loggedIn"
-    variant="outlined"
-    rounded
-    size="small"
-    @click="createStory"
+  <v-tooltip
+    :disabled="loggedIn"
+    location="top"
   >
-    <v-icon small>
-      {{ mdiPlus }}
-    </v-icon>
-    {{ t("additional:modules.dataNarrator.create") }}
-  </v-btn>
+    <template #activator="{ props }">
+      <span v-bind="props">
+        <v-btn
+          variant="outlined"
+          rounded
+          size="small"
+          :disabled="!loggedIn"
+          @click="createStory"
+        >
+          <v-icon small>
+            {{ mdiPlus }}
+          </v-icon>
+          {{ t("additional:modules.dataNarrator.create") }}
+        </v-btn>
+      </span>
+    </template>
+    {{ t("additional:modules.dataNarrator.loginToCreate") }}
+  </v-tooltip>
 </template>
 
