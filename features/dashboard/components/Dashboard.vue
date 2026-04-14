ea522a27ccdc216cc9955c033c3f03fc30824028
illsio <smasho@web.de>
2026-04-14 12:50:30 +0200

    logo hide fix try II - rewind Dashboard.vue

--- features/dashboard/components/Dashboard.vue
+++ features/dashboard/components/Dashboard.vue
@@ -18,18 +18,5 @@ import DashboardHeader from './DashboardHeader.vue';
   </ToolWindow>
 </template>
 
-<template>
-  <ToolWindow>
-    <template #header>
-      <DashboardHeader />
-    </template>
-    <template #default>
-      <div id="tool-dataNarrator-creator-stepForm">
-        <DashboardContent />
-      </div>
-    </template>
-  </ToolWindow>
-</template>
-
 <style>
 </style>
