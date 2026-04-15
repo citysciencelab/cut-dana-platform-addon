eadc038799f0ae2d8c369cde296bf85e05fc2e5a
illsio <smasho@web.de>
2026-04-15 09:35:35 +0200

    refactoring new files - forgot

---
+++ hooks/useNavigation.js
@@ -0,0 +1,96 @@
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex';
+
+export function useNavigation() {
+  const store = useStore();
+
+  const zoom = computed(() => store.state.Maps.zoom);
+  const center = computed(() => store.state.Maps.center);
+
+  const initialZoom = ref(store.state.Maps.zoom);
+  const initialCenter = ref(store.state.Maps.center);
+
+  const canZoomIn = computed(() => !store.getters['Maps/isMaxZoomDisplayed']);
+  const canZoomOut = computed(() => !store.getters['Maps/isMinZoomDisplayed']);
+
+  const zoomIn = () => store.dispatch('Maps/increaseZoom');
+  const zoomOut = () => store.dispatch('Maps/decreaseZoom');
+
+  const setView = ({ center, zoom }) => {
+    store.commit('Maps/setView', {
+      center,
+      zoom
+    });
+  }
+
+  const setAnimatedView = ({ center, zoom }) => {
+    store.commit('Maps/animateView', {
+      center,
+      zoom
+    });
+  }
+
+  const setBaseLayer = (layerId) => {
+    if (!layerId) {
+      return
+    }
+
+    store.dispatch('Modules/BaselayerSwitcher/updateLayerVisibilityAndZIndex', layerId);
+  };
+
+  const setInformationLayers = (layers = [], removableBlackList = [ '19969' ]) => {
+    const layerIds = layers.map(({ id }) => id);
+    const prev = store.getters.visibleLayerConfigs.map(config => config.id) || [];
+
+    const added = layerIds.filter(id => !prev.includes(id));
+    const removed = prev.filter(id => !layerIds.includes(id));
+
+    for (const id of added) {
+      store.dispatch('addOrReplaceLayer', {
+        layerId: id,
+        isBaseLayer: false,
+        transparency: layers.find(item => item.id === id)?.transparency
+      });
+    }
+
+    for (const id of removed) {
+      if (removableBlackList.includes(id)) {
+        continue;
+      }
+      store.dispatch('Modules/LayerTree/removeLayer', { id });
+    }
+  }
+
+  const removeAllVisibleLayers = (restoreDefaultBaseLayer = true) => {
+    const defaultBaseLayerId = '19969';
+
+    if (restoreDefaultBaseLayer) {
+      setBaseLayer(defaultBaseLayerId);
+    }
+
+    for (const layerConfig of store.getters.visibleLayerConfigs) {
+      const id = layerConfig.id;
+      if (restoreDefaultBaseLayer && id === defaultBaseLayerId) {
+        continue;
+      }
+      store.dispatch('Modules/LayerTree/removeLayer', { id });
+    }
+  }
+
+  return {
+    canZoomIn,
+    canZoomOut,
+    center,
+    removeAllVisibleLayers,
+    defaultBaseLayerId: '19969',
+    initialCenter,
+    initialZoom,
+    setAnimatedView,
+    setBaseLayer,
+    setInformationLayers,
+    setView,
+    zoom,
+    zoomIn,
+    zoomOut,
+  }
+}
